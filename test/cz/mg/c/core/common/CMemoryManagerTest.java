c7534a1b4c8b25c3fc7792a014bd62c21f63a6ea
mg <gekoncze@seznam.cz>
2025-05-19 22:34:46 +0200

    extended memory manager test

--- test/cz/mg/c/core/common/CMemoryManagerTest.java
+++ test/cz/mg/c/core/common/CMemoryManagerTest.java
@@ -4,6 +4,8 @@ import cz.mg.c.core.CLibrary;
 import cz.mg.c.core.Configuration;
 import cz.mg.test.Assert;
 
+import static cz.mg.c.core.entities.CPointer.NULL;
+
 public class CMemoryManagerTest {
     public static void main(String[] args) {
         System.out.print("Running " + CMemoryManagerTest.class.getSimpleName() + " ... ");
@@ -25,12 +27,14 @@ public class CMemoryManagerTest {
         try (manager) {
             long firstAllocation = manager.allocate(2L);
 
+            Assert.assertNotEquals(NULL, firstAllocation);
             Assert.assertEquals(1L, manager.getAllocations().count());
             Assert.assertEquals(firstAllocation, manager.getAllocations().getFirst());
             Assert.assertEquals(2L, manager.getTotalSize());
 
             long secondAllocation = manager.allocate(5L);
 
+            Assert.assertNotEquals(NULL, secondAllocation);
             Assert.assertEquals(2L, manager.getAllocations().count());
             Assert.assertEquals(secondAllocation, manager.getAllocations().getLast());
             Assert.assertEquals(7L, manager.getTotalSize());
