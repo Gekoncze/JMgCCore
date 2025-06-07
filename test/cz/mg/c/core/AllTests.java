0073cf0699505493f04df23fa276f5130bb7f54b
mg <gekoncze@seznam.cz>
2025-06-07 17:26:17 +0200

    added missing test

--- test/cz/mg/c/core/AllTests.java
+++ test/cz/mg/c/core/AllTests.java
@@ -5,6 +5,7 @@ import cz.mg.c.core.common.*;
 import cz.mg.c.core.types.CArrayTest;
 import cz.mg.c.core.types.CObjectTest;
 import cz.mg.c.core.types.CPointerTest;
+import cz.mg.c.core.types.elementary.CCharTest;
 
 public @Test class AllTests {
     public static void main(String[] args) {
@@ -16,9 +17,12 @@ public @Test class AllTests {
         CMemoryManagerTest.main(args);
         CMemoryTest.main(args);
 
-        // cz.mg.c.core.entities
+        // cz.mg.c.core.types
         CArrayTest.main(args);
         CObjectTest.main(args);
         CPointerTest.main(args);
+
+        // cz.mg.c.core.types.elementary
+        CCharTest.main(args);
     }
 }
