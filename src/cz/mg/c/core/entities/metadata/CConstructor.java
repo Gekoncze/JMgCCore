2baa99e2dba249e22d38c6b749a220983c8baa87
mg <gekoncze@seznam.cz>
2025-06-07 10:37:50 +0200

    updated type parameters

--- src/cz/mg/c/core/entities/metadata/CConstructor.java
+++ src/cz/mg/c/core/entities/metadata/CConstructor.java
@@ -2,7 +2,8 @@ package cz.mg.c.core.entities.metadata;
 
 import cz.mg.annotations.classes.Component;
 import cz.mg.annotations.requirement.Mandatory;
+import cz.mg.c.core.entities.CObject;
 
-public @Component interface CConstructor<C> {
+public @Component interface CConstructor<C extends CObject> {
     @Mandatory C create(long address);
 }
--- src/cz/mg/c/core/entities/metadata/CMetadata.java
+++ src/cz/mg/c/core/entities/metadata/CMetadata.java
@@ -1,6 +1,7 @@
 package cz.mg.c.core.entities.metadata;
 
 import cz.mg.annotations.requirement.Mandatory;
+import cz.mg.c.core.entities.CObject;
 
-public record CMetadata<T>(long size, @Mandatory CConstructor<T> constructor) {
+public record CMetadata<C extends CObject>(long size, @Mandatory CConstructor<C> constructor) {
 }
